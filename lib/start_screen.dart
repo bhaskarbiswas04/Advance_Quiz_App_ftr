32b20b6994c30e437ed972e835762e964b1ef75c
bhaskarbiswas04 <bhaskarb45nov@gmail.com>
2024-07-17 21:35:35 +0530

    update

---
+++ lib/start_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.purple],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                height: 250,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              const Text(
+                'Learn flutter the fun way!',
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.forward,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 80, 12, 92),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 30),
+                  ),
+                  label: const Text(
+                    'Start Quiz',
+                    style: TextStyle(fontSize: 25, color: Colors.white),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
